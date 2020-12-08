e1c4d0f4d18ff037d15dff119f55b5cbbb486d5f
achiang <jecky_hindrawan123@yahoo.com>
2020-12-08 20:50:55 +0700

    susah dilanjut karena api public nya ga sesuai dan ga cocok

---
+++ Resto/src/Screen/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Countries() {
+  return (
+    <View>
+      <Text>aaaaa</Text>
+      <Text>susah dilanjut karena api public nya ga sesuai dan ga cocok</Text>
+    </View>
+  );
+}
--- Resto/src/route/Stack/StackScreen.js
+++ Resto/src/route/Stack/StackScreen.js
@@ -6,18 +6,20 @@ import Regis from '../../Screen/Regis/Regis';
 import Verify from '../../Screen/Verify/Verify';
 import Forgot from '../../Screen/Forgot/Forgot';
 import OnBoarding from '../../Screen/OnBoarding/OnBoarding';
+import Countries from '../../Screen/Countries/Countries';
 
 const Stack = createStackNavigator();
 
 export default function StackScreen() {
   return (
     <Stack.Navigator headerMode={'none'}>
+      <Stack.Screen name="Countries" component={Countries} />
+      <Stack.Screen name="TabScreen" component={TabScreen} />
       <Stack.Screen name="OnBoarding" component={OnBoarding} />
       <Stack.Screen name="Regis" component={Regis} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Verify" component={Verify} />
       <Stack.Screen name="Forgot" component={Forgot} />
-      <Stack.Screen name="TabScreen" component={TabScreen} />
     </Stack.Navigator>
   );
 }
